4318af8dc92ce4245a978f6685eb9047dd630efd
AijazQA <sap.aijaz@gmail.com>
2026-01-23 18:57:54 +0530

    UserPostForSocialNetwork

---
+++ Post.java
@@ -0,0 +1,15 @@
+public class Post {
+    private String content;
+
+    public Post(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
